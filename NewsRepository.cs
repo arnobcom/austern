45617960f36637dcfce7fd9911ec07966b1c3df6
arnobcom <arnobcom@yahoo.com>
2013-10-16 19:35:16 +1100

    Update NewsRepository.cs

--- NewsRepository.cs
+++ NewsRepository.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EdgeCorp.Repository
+namespace Austern.Repository
 {
     public class NewsRepository:BaseRepository 
     {
